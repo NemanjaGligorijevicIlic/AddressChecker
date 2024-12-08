29960de575fa27d5f9489c22da77f352da401bdf
NemanjaGligorijevicIlic <nemgli-8@student.ltu.se>
2024-12-08 20:26:06 +0100

    Address checker

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "zoo.tours.hybrid"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.seleniumhq.selenium:selenium-java:4.13.0")
+    implementation("io.github.bonigarcia:webdrivermanager:5.5.3")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
